61d725a638c2c1ffe31507161b3302b04429fa1c
Jason Rebelo Neves <igotinfected@gmail.com>
2022-12-11 22:23:04 +0100

    :sparkles: add bootstrap & nerd-font install scripts
    Also includes first attempt at syncing these to their respective gists
    via a workflow.

---
+++ gists/bootstrap-linux.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# install favourite nerd font
+curl -sS https://gist.githubusercontent.com/igotinfected/b86359da8e64fd3cfd1fa11fbcbb36ba/raw/install-nerd-font.sh
+
+# install starship
+curl -sS https://starship.rs/install.sh | sh
---
+++ gists/install-nerd-font.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# ensure fontconfig pkg is installed
+echo "Ensuring fontconfig package is installed..."
+$(dpkg-query -W -f='${Status}' nano 2>/dev/null | grep -c "ok installed") \
+	|| sudo apt install fontconfig -y
+
+# download the font
+echo "Downloading the font..."
+wget "https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/FiraCode/Regular/complete/Fira%20Code%20Regular%20Nerd%20Font%20Complete%20Mono.ttf"
+
+# create font directory if it doesn't exist
+echo "Creating font directory..."
+mkdir -p $HOME/.local/share/fonts
+
+# install the font
+echo "Installing the font..."
+mv "Fira Code Regular Nerd Font Complete Mono.ttf" $HOME/.local/share/fonts
+
+# refresh font cache
+echo "Refreshing font cache..."
+fc-cache -fv
+
+echo "✅ done!"
