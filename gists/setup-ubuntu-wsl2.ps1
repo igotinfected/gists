12719a3bdbdeac1282f8ef7fbf537ad0042598b4
Jason Rebelo Neves <igotinfected@gmail.com>
2022-12-12 23:49:49 +0100

    :sparkles: add WSL2 + ubuntu distro setup

---
+++ gists/setup-ubuntu-wsl2.ps1
@@ -0,0 +1,20 @@
+# based on https://github.com/Alex-D/dotfiles#setup-wsl-2
+# run as admin in pwsh
+
+# enable wsl + features
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+
+# download & install kernel update
+$wslUpdateInstallerUrl = "https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi"
+$downloadFolderPath = (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path
+$wslUpdateInstallerFilePath = "$downloadFolderPath/wsl_update_x64.msi"
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile($wslUpdateInstallerUrl, $wslUpdateInstallerFilePath)
+Start-Process -Filepath "$wslUpdateInstallerFilePath"
+
+# set wsl2 as default
+wsl --set-default-version 2
+
+# install default ubuntu distro
+wsl --install Ubuntu
